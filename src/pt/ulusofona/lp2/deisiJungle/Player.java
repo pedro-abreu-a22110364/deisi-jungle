904a67ad427e66be8ab3ac6ba21e0bc6d998316e
Guilherme <guigasthepro@gmail.com>
2022-11-05 13:37:14 +0000

    Implementing new methods in Player class

--- src/pt/ulusofona/lp2/deisiJungle/Player.java
+++ src/pt/ulusofona/lp2/deisiJungle/Player.java
@@ -1,5 +1,7 @@
 package pt.ulusofona.lp2.deisiJungle;
 
+import java.util.ArrayList;
+
 public class Player {
     int identifier;
     String nome;
@@ -9,6 +11,22 @@ public class Player {
         this.identifier = identifier;
         this.nome = nome;
         this.specie = specie;
+    }
+
+    public boolean isAlreadyTarzanPlaying(ArrayList<Player> alPlayers)
+    {
+        for (Player player : alPlayers) {
+            if(player.specie.identifier == 'Z'){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean checkID(int identifier)
+    {
+        return this.identifier + 1 != identifier;
     }
 
 }
