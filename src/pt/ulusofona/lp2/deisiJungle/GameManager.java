f2fa7639ceef3e241886894f6aacf4985b1c893a
PedroAbreu11 <pedromiguelrijodeabreu@gmail.com>
2022-11-06 16:35:16 +0000

    GetSpecies

--- src/pt/ulusofona/lp2/deisiJungle/GameManager.java
+++ src/pt/ulusofona/lp2/deisiJungle/GameManager.java
@@ -5,15 +5,26 @@ import java.util.ArrayList;
 
 public class GameManager {
 
+    int minPlayers = 2;
     int maxPlayers = 4;
-
+    ArrayList<Species> alSpecies = createDefaultSpecies();
 
     public GameManager(){
 
     }
 
     public String[][] getSpecies() {
-        return new String[5][];
+        String[][] species = new String[alSpecies.size()][3];
+        int count = 0;
+
+        for (Species specie : alSpecies) {
+            species[count][0] = specie.getIdentifier() + "";
+            species[count][1] = specie.getNome();
+            species[count][2] = specie.getSpecieImage() + "";
+            count++;
+        }
+
+        return species;
     }
 
     public boolean createInitialJungle(int jungleSize,int initialEnergy,String[][] playersInfo) {
@@ -58,6 +69,27 @@ public class GameManager {
 
     public String whoIsTaborda() {
         return "";
+    }
+
+    public ArrayList<Species> createDefaultSpecies() {
+        ArrayList<Species> alSpecies = new ArrayList<>(); //Creating the list to return it later
+
+        //Creating the objects
+        Species elefante = new Species('E', "Elefante");
+        Species leao = new Species('L', "Leão");
+        Species tartaruga = new Species('T', "Tartaruga");
+        Species passaro = new Species('P', "Pássaro");
+        Species tarzan = new Species('Z', "Tarzan");
+
+        //Adding objects to list
+        alSpecies.add(elefante);
+        alSpecies.add(leao);
+        alSpecies.add(tartaruga);
+        alSpecies.add(passaro);
+        alSpecies.add(tarzan);
+
+        //Returning the list back to "main"
+        return alSpecies;
     }
 
 }
--- src/pt/ulusofona/lp2/deisiJungle/Species.java
+++ src/pt/ulusofona/lp2/deisiJungle/Species.java
@@ -15,35 +15,16 @@ public class Species {
         //this.specieImage = specieImage;
     }
 
-    public ArrayList<Species> createDefaultSpecies() {
-        ArrayList<Species> alSpecies = new ArrayList<>(); //Creating the list to return it later
-
-        //Creating the objects
-        Species elefante = new Species('E', "Elefante");
-        Species leao = new Species('L', "Leão");
-        Species tartaruga = new Species('T', "Tartaruga");
-        Species passaro = new Species('P', "Pássaro");
-        Species tarzan = new Species('Z', "Tarzan");
-
-        //Adding objects to list
-        alSpecies.add(elefante);
-        alSpecies.add(leao);
-        alSpecies.add(tartaruga);
-        alSpecies.add(passaro);
-        alSpecies.add(tarzan);
-
-        //Returning the list back to "main"
-        return alSpecies;
-    }
-
     public char getIdentifier()
     {
         return identifier;
     }
+
     public String getNome()
     {
         return nome;
     }
+
     public Image getSpecieImage()
     {
         return specieImage;
