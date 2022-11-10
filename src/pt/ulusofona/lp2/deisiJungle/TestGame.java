1fa4f17695a7b075d537942388145ff61abe66b2
PedroAbreu11 <pedromiguelrijodeabreu@gmail.com>
2022-11-10 16:37:24 +0000

    added test to getPlayersInfo

--- src/pt/ulusofona/lp2/deisiJungle/TestGame.java
+++ src/pt/ulusofona/lp2/deisiJungle/TestGame.java
@@ -2,6 +2,12 @@ package pt.ulusofona.lp2.deisiJungle;
 
 import org.junit.Test;
 
+import java.util.Objects;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 public class TestGame {
     @Test
     public void testCreateJungle()
@@ -17,6 +23,24 @@ public class TestGame {
         };
         manager.createInitialJungle(20, 10, playerInfo);
 
+    }
+
+    @Test
+    public void testGetPlayersInfo()
+    {
+        GameManager manager = new GameManager();
+        String[][] playerInfo = new String[][]{
+                {
+                        "1","Pedro","Z","10"
+                },
+                {
+                        "2","Guilherme","T","10"
+                }
+        };
+        manager.createInitialJungle(20, 10, playerInfo);
+        String[][] matriz = manager.getPlayersInfo();
+
+        assertEquals("1", matriz[0][0]);
     }
 
 }
